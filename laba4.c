07100268a99a51baf65d441bd49a7f881a3146cf
daubilnik <121643997+danelanchik@users.noreply.github.com>
2023-04-03 01:25:19 +0500

    Create laba4.c
    не уверен в контрольном значении интеграла, поэтому сидел прогонял всё

---
+++ laba4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+double func(double a) {
+    if(a>=0 && a<=1) return cos(a*M_PI/180)*exp(pow(-a,2));
+    else if(a>1 && a<=2) return log(a+1)-sqrt(4-pow(a,2));
+    else return 0.0;
+}
+
+double runge(double a, double b) { 
+    return (a-b)/3;
+}
+int main() {
+    double x=0, I1=0, I2=0;
+    int n=pow(2,10);
+    double h = 2./n;
+    for(int i=0;i<=n;i++) {
+        I1+=func(x+i*h);
+    }
+    for(int i=0;i<=n*2;i++) {
+        I2+=func(x+i*(h/2));
+    }
+    I1*=h; I2*=h/2;
+    printf("%lf\t%lf", I1, I2);
+    return 0;
+}
