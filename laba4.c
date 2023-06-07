93d4daaae851b20947688ef0ff54ef38911e3c52
daubilnik <121643997+danelanchik@users.noreply.github.com>
2023-06-07 11:47:58 +0500

    Update laba4.c

--- laba4.c
+++ laba4.c
@@ -2,30 +2,35 @@
 #include <math.h>
 
 double func(double a) {
-    if(a>=0 && a<=1.000001) return cos(a)*exp(-pow(a,2));
-    else if(a>1.000001 && a<=2) return log(a+1)-sqrt(4-pow(a,2));
+    if(a>=0 && a<=1) return cos(a)*exp(-pow(a,2));
+    else if(a>1 && a<=2) return log(a+1)-sqrt(4-pow(a,2));
     else return 0.0;
 }
 
 double runge(double a, double b) { 
     return (a-b)/3;
 }
+
 int main() {
     double eps;
     printf("Print innacuracy max rate -> ");
     scanf("%lf", &eps);
+    
     double x=0, I1=0, I2=0;
     for(int i=0;i<=30;i++){
         long int n=pow(2,i);
         double h = 2./n;
+        
         for(int i=0;i<=n;i++) {
             I1+=func(x+i*h/2);
         }
+        
         for(int i=0;i<=n*2;i++) {
             I2+=func(x+i*(h/4));
         }
         I1*=h; I2*=h/2;
-        printf("if n = %ld, runge innacuracy = %.7lf\n", n,  runge(I1,I2));
+        
+        printf("if n = %ld, runge innacuracy = %.7lf, I= %.7lf\n", n,  runge(I1,I2),I2);
         if(runge(I1,I2)<eps) return 0;
     }
     return 0;
