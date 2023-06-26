d33b9599efa521ad72f80fefcbd9d3d965480140
daubilnik <121643997+danelanchik@users.noreply.github.com>
2023-06-27 01:43:58 +0500

    Update laba9.c

--- laba9.c
+++ laba9.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
+#include <ctype.h>
+
+int main() {
     char str[200];
-    for(int i=0;i<200;i++) {
-        scanf("%c", &str[i]);
-        printf("%c", str[i]);
-        if(str[i] == '\n') break;
-    }
-    char * leks = strtok(str, " ");
+    printf("Введите любые слова через пробел, завершить ввод нажатием ENTER: ");
+    fgets(str, 200, stdin);
+    char *leks = strtok(str, " ");
     int count = 0;
-    while(leks != NULL) {
-        printf("%c\n", leks[count]);
-        count++;
+    while (leks != NULL) {
+        int len = strlen(leks);
+        for (int i = 0; i < len; i++) {
+            if ((i == count) && (isalpha(leks[i]))) {
+                printf("%c\n", leks[i]);
+            }
+        }
         leks = strtok(NULL, " ");
+        count++;
     }
     return 0;
 }
